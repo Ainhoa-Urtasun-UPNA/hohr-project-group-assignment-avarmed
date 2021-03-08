416d11bb16f94ef213dd0912b00cb8af0b0f0c95
ruthpascualgarc <pascual.126749@e.unavarra.es>
2021-03-08 15:23:07 +0000

    Results of tasks 3 and 4

--- HRplanning.py
+++ HRplanning.py
@@ -1,20 +1,27 @@
 import numpy
 
 # Forecasting number of available workers
-D = numpy.array([[],[],[]])
-M = numpy.array([[],[],[]])
-b = numpy.array([[],[],[]])
+print("\n", "TASK 3")
+D = numpy.array([[15,0,0],[0,20,0],[0,0,50]])
+M = numpy.array([[5,2,0],[0,10,0],[5,5,45]])
+b = numpy.array([[1],[2],[2]])
 a = M@numpy.linalg.inv(D)@b
-print(a)
+print("a=", "\n", a, "\n")
+#program credits: Ruti:)
 
 # Forecasting number of needed workers
-X = numpy.array([[],[],[],[],[],[],[],[],[]])
-Y = numpy.array([[],[],[],[],[],[],[],[],[]])
+print("\n","TASK 4", "\n")
+X = numpy.array([[15,1,0,0],[17,1,0,0],[14,1,0,0],[6,0,1,0],[6,0,1,0],[5,0,1,0],[23,0,0,1],[27,0,0,1],[25,0,0,1]])
+Y = numpy.array([[4],[6],[5],[7],[7],[9],[13],[12],[13]])
 XTX = numpy.matmul(X.T,X)
-print(XTX)
+print(XTX, "\n")
 inverse = numpy.linalg.inv(XTX)
-print(inverse)
+print(inverse, "\n")
 XTY = numpy.matmul(X.T,Y)
-print(XTY)
+print(XTY, "\n")
 betas = numpy.matmul(inverse,XTY)
-print(betas)
+print(betas, "\n")
+U = numpy.array([[10,1,0,0],[4,0,1,0],[20,0,0,1]])
+BU = numpy.matmul(U, betas)
+print("\n", "BU=", "\n", BU)
+#program credits: Ruti:)
